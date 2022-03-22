43f50c08234fab7bc11cce0466cef1659b0cc48a
renato-castillo <renatocastillopingo@gmail.com>
2022-03-22 09:52:40 -0500

    Added endpoint

--- src/main/java/com/bootcamp/accounttransactions/controller/TransferController.java
+++ src/main/java/com/bootcamp/accounttransactions/controller/TransferController.java
@@ -1,9 +1,15 @@
 package com.bootcamp.accounttransactions.controller;
 
+import com.bootcamp.accounttransactions.dto.MovementDto;
+import com.bootcamp.accounttransactions.dto.TransactionDto;
+import com.bootcamp.accounttransactions.dto.TransferDto;
 import com.bootcamp.accounttransactions.resource.TransferRecordResource;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
 
 @RestController
 @RequestMapping("/api/transfer")
@@ -12,6 +18,10 @@ public class TransferController {
     @Autowired
     private TransferRecordResource transferRecordResource;
 
+    @PostMapping
+    public Mono<TransactionDto> create(@RequestBody TransferDto transferDto) {
+        return transferRecordResource.create(transferDto);
+    }
 
 
 }
--- src/main/java/com/bootcamp/accounttransactions/resource/TransferRecordResource.java
+++ src/main/java/com/bootcamp/accounttransactions/resource/TransferRecordResource.java
@@ -29,7 +29,7 @@ public class TransferRecordResource extends MapperUtil {
     @Autowired
     private TransferCreationService transferCreationService;
 
-    public Mono<TransactionDto> createMovement(TransferDto transferDto) {
+    public Mono<TransactionDto> create(TransferDto transferDto) {
 
         Mono<TransferRecord> monoTransfer;
 
